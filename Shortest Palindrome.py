4d641487ebf5f6ed381ae93a485fd314a92ebef5
nithisharul <Nithishap2006@gmail.com>
2026-01-03 19:03:58 +0400

    Implement shortest palindrome solution

---
+++ Shortest Palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def shortestPalindrome(self, s):
+        rev = s[::-1]
+        l = s + "#" + rev
+        p = [0] * len(l)
+
+        for i in range(1, len(l)):
+            j = p[i-1]
+            while j > 0 and l[i] != l[j]:
+                j = p[j-1]
+            if l[i] == l[j]:
+                j += 1
+            p[i] = j
+
+        return rev[:len(s) - p[-1]] + s
